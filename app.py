87263ba48114b50443abcbc5f90c8d64e9ddf807
akshay AS <akshay@akshays-Laptop.local>
2024-08-07 23:54:24 +0530

    adding files

---
+++ app.py
@@ -0,0 +1,89 @@
+import streamlit as st
+from lyzr_automata.ai_models.openai import OpenAIModel
+from lyzr_automata import Agent, Task
+from lyzr_automata.pipelines.linear_sync_pipeline import LinearSyncPipeline
+from PIL import Image
+from lyzr_automata.tasks.task_literals import InputType, OutputType
+import os
+
+# Set the OpenAI API key
+os.environ["OPENAI_API_KEY"] = st.secrets["apikey"]
+
+st.markdown(
+    """
+    <style>
+    .app-header { visibility: hidden; }
+    .css-18e3th9 { padding-top: 0; padding-bottom: 0; }
+    .css-1d391kg { padding-top: 1rem; padding-right: 1rem; padding-bottom: 1rem; padding-left: 1rem; }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+image = Image.open("./logo/lyzr-logo.png")
+st.image(image, width=150)
+
+# App title and introduction
+st.title("Financial Literacy Assistant")
+st.markdown("Welcome to our Financial Literacy Assistant, where you can ask questions and learn about managing your finances effectively. Explore and grow your financial confidence with our easy-to-use app.")
+st.markdown(" **Let us know any concerns or questions you like us to address?**")
+input = st.text_input(" Please enter the above details:",placeholder=f"""Type here""")
+
+open_ai_text_completion_model = OpenAIModel(
+    api_key=st.secrets["apikey"],
+    parameters={
+        "model": "gpt-4-turbo-preview",
+        "temperature": 0.2,
+        "max_tokens": 1500,
+    },
+)
+
+
+def generation(input):
+    generator_agent = Agent(
+        role="Expert FINANCIAL ADVISOR",
+        prompt_persona=f"Your task is to EXPLAIN and EDUCATE users on PERSONAL FINANCE topics or questions. You MUST make the response as UNDERSTANDABLE and INFORMATIVE as possible, directly addressing the questions the user asks.")
+    prompt = f"""
+You are an Expert FINANCIAL ADVISOR. Your task is to EXPLAIN and EDUCATE users on PERSONAL FINANCE topics or questions. You MUST make the response as UNDERSTANDABLE and INFORMATIVE as possible, directly addressing the questions the user asks.
+
+Here's your step-by-step guide:
+
+1. LISTEN carefully to the user's question to fully understand their personal finance inquiry.
+
+2. BREAK DOWN complex financial concepts into SIMPLE, easy-to-understand language.
+
+3. PROVIDE CLEAR and CONCISE explanations that are tailored to the user's level of financial knowledge.
+
+4. ENSURE that you cover all aspects of the question, leaving no part unanswered.
+
+
+ """
+
+    generator_agent_task = Task(
+        name="Generation",
+        model=open_ai_text_completion_model,
+        agent=generator_agent,
+        instructions=prompt,
+        default_input=input,
+        output_type=OutputType.TEXT,
+        input_type=InputType.TEXT,
+    ).execute()
+
+    return generator_agent_task 
+   
+if st.button("Assist!"):
+    solution = generation(input)
+    st.markdown(solution)
+
+with st.expander("ℹ️ - About this App"):
+    st.markdown("""
+    This app uses Lyzr Automata Agent . For any inquiries or issues, please contact Lyzr.
+
+    """)
+    st.link_button("Lyzr", url='https://www.lyzr.ai/', use_container_width=True)
+    st.link_button("Book a Demo", url='https://www.lyzr.ai/book-demo/', use_container_width=True)
+    st.link_button("Discord", url='https://discord.gg/nm7zSyEFA2', use_container_width=True)
+    st.link_button("Slack",
+                   url='https://join.slack.com/t/genaiforenterprise/shared_invite/zt-2a7fr38f7-_QDOY1W1WSlSiYNAEncLGw',
+                   use_container_width=True)
+    
